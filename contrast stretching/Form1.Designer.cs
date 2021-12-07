f705c1905525e7b5cd6acd96da5c18b7bc308a2f
aeg13 <aeg13a@gmail.com>
2021-12-07 21:44:56 +0300

    Add project files.

---
+++ contrast stretching/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace contrast_stretching
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(91, 73);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(623, 470);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::contrast_stretching.Properties.Resources.İnci_Küpeli_Kız___Vermeer_1024x1024;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 31);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(555, 413);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(759, 73);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(614, 470);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(29, 31);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(557, 413);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(682, 559);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kontrast Germe";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1386, 639);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ contrast stretching/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contrast_stretching
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap orijinal = new Bitmap(pictureBox1.Image);
+
+            byte[] rKanaliRenkDizisi = new byte[256];
+            byte[] gKanaliRenkDizisi = new byte[256];
+            byte[] bKanaliRenkDizisi = new byte[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                rKanaliRenkDizisi[i] = gKanaliRenkDizisi[i] = bKanaliRenkDizisi[i] = 0;
+            }
+
+            for (int x = 0; x < orijinal.Width; x++)
+            {
+                for (int y = 0; y < orijinal.Height; y++)
+                {
+                    rKanaliRenkDizisi[orijinal.GetPixel(x, y).R]++;
+                    gKanaliRenkDizisi[orijinal.GetPixel(x, y).G]++;
+                    bKanaliRenkDizisi[orijinal.GetPixel(x, y).B]++;
+                }
+            }
+
+            int rMax, gMax, bMax;
+            rMax = gMax = bMax = 0;
+
+            for (int i = 255; i >= 0; i--)
+            {
+                if (rKanaliRenkDizisi[i] != 0)
+                {
+                    rMax = i; break;
+                }
+            }
+            for (int i = 255; i >= 0; i--)
+            {
+                if (gKanaliRenkDizisi[i] != 0)
+                {
+                    gMax = i; break;
+                }
+            }
+            for (int i = 255; i >= 0; i--)
+            {
+                if (bKanaliRenkDizisi[i] != 0)
+                {
+                    bMax = i; break;
+                }
+            }
+            int rMin, gMin, bMin;
+            rMin = gMin = bMin = 0;
+
+            for (int i = 0; i < 256; i++)
+            {
+                if (rKanaliRenkDizisi[i] != 0)
+                {
+                    rMin = i; break;
+                }
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                if (gKanaliRenkDizisi[i] != 0)
+                {
+                    gMin = i; break;
+                }
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                if (bKanaliRenkDizisi[i] != 0)
+                {
+                    bMin = i; break;
+                }
+            }
+
+            int gmin = 0; int gmax = 255;
+            double rCikti, gCikti, bCikti;
+            rCikti = gCikti = bCikti = 0;
+
+            double rCarpan = 0; double gCarpan = 0; double bCarpan = 0;
+            Bitmap cikti = new Bitmap(orijinal.Width, orijinal.Height);
+
+            for (int x = 0; x < orijinal.Width; x++)
+            {
+                for (int y = 0; y < orijinal.Height; y++)
+                {
+                    rCarpan = ((double)(gmax - gmin) / (double)(rMax - rMin));
+                    rCikti = (double)((orijinal.GetPixel(x, y).R - rMin) * (rCarpan) + gmin);
+                    gCarpan = (double)(gmax - gmin) / (double)(gMax - gMin);
+                    gCikti = (double)((orijinal.GetPixel(x, y).G - gMin) * (gCarpan) + gmin);
+                    bCarpan = (double)(gmax - gmin) / (double)(bMax - bMin);
+                    bCikti = (double)((orijinal.GetPixel(x, y).B - bMin) * (bCarpan) + gmin);
+                    cikti.SetPixel(x, y, Color.FromArgb((int)rCikti, (int)bCikti, (int)bCikti));
+                }
+            }
+            pictureBox2.Image = cikti;
+        }
+    
+    }
+}
